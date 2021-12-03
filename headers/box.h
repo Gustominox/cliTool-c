09292835aa0cf651a53544b5e2e9d33949f40999
Gustominox <agstcmps.111@gmail.com>
2021-12-03 17:01:07 +0000

    Add headers to repo

---
+++ headers/box.h
@@ -0,0 +1,27 @@
+#ifndef BOX_H_
+#define BOX_H_
+
+#include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+void printHead(size_t col, FILE *output);
+
+void printClose(size_t col, FILE *output);
+
+void printHBorder(size_t h_border, FILE *output);
+
+void printEmptyLine(size_t col, FILE *output);
+
+void printVBorder(size_t v_border, size_t col, FILE *output);
+
+size_t strlen_unicode(char string[]);
+
+void printLine(char line[], size_t h_border, size_t maxArgsSize, FILE *output);
+
+size_t maxArgsSize(char **args);
+
+void printBox(char text[], size_t h_border, size_t v_border, FILE *output);
+
+
+#endif // BOX_H_
---
+++ headers/unicode.h
@@ -0,0 +1,21 @@
+#ifndef UNICODE_H_
+#define UNICODE_H_
+
+#define STAR        "\xe2\x9c\xaf"    /* Star        */
+#define THIN_TICK   "\xE2\x9C\x93"    /* Thin Tick   */
+#define BOLD_TICK   "\xE2\x9C\x94"    /* Bold Tick   */
+#define UNICODE_X   "\xe2\x9c\x96"    /* X           */
+#define UP_ARROW    "\xe2\x86\x91"    /* UP_ARROW    */
+#define DOWN_ARROW  "\xe2\x86\x93"    /* DOWN_ARROW  */
+#define LEFT_ARROW  "\xe2\x86\x90"    /* LEFT_ARROW  */
+#define RIGHT_ARROW "\xe2\x86\x92"    /* RIGHT_ARROW */
+#define H_L   "\xe2\x94\x80"    /* Horizontal Line    */
+#define V_L   "\xe2\x94\x82"    /* Vertical Line      */
+#define T_L_A "\xe2\x94\x8c"    /* Top Left Angle     */
+#define T_R_A "\xe2\x94\x90"    /* Top Right Angle    */
+#define B_L_A "\xe2\x94\x94"    /* Bottom Left Angle  */
+#define B_R_A "\xe2\x94\x98"    /* Bottom Right Angle */
+
+#endif // UNICODE_H_
+
+/* REF: https://www.utf8-chartable.de/unicode-utf8-table.pl */
