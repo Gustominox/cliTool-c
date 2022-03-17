42731d86674acb1012e8287ad578b9a6d0899d19
Gustominox <agstcmps.111@gmail.com>
2022-03-17 15:21:37 +0000

    MakeBox working Dynamically
    Returns a Dynamic string that contains the box specified in the function arguments
    Valgrind reports no error's or memory leaks

--- src/box.c
+++ src/box.c
@@ -4,53 +4,97 @@
 #include "fruits.h"
 #include "ball.h"
 
-void printHead(size_t col, FILE *output)
-{
 
-    fprintf(output, T_L_A);
-    for (size_t i = 0; i < col; i++)
+char *strcatrealloc(char *str1, char str2[])
+{
+    size_t str1len = 0;
+    if (str1)
     {
-        fprintf(output, H_L);
+        str1len = strlen(str1);
     }
-    fprintf(output, T_R_A);
-    fputc('\n', output);
+    size_t str2len = strlen(str2);
+
+    // char *strdest = (char *)malloc(sizeof(char));
+    // It's easy to forget that strlen doesn't count the
+    // trailing NULL character,
+    size_t neededbufferlen = str1len + str2len + 1;
+
+
+    char *strdest = (char *)malloc(neededbufferlen * sizeof(char));
+    if (strdest)
+    {
+        if (str1)
+        {
+            strcpy(strdest, str1);
+            strcat(strdest, str2);
+        }
+        else
+        {
+            strcpy(strdest, str2);
+        }
+    }
+
+    if (str1)
+        free(str1);
+
+    return strdest;
 }
 
-void printClose(size_t col, FILE *output)
+char *printHead(size_t col, char *output)
 {
-
-    fprintf(output, B_L_A);
+    output = strcatrealloc(output, T_L_A);
     for (size_t i = 0; i < col; i++)
     {
-        fprintf(output, H_L);
+        output = strcatrealloc(output, H_L);
     }
-    fprintf(output, B_R_A);
-    fputc('\n', output);
+    output = strcatrealloc(output, T_R_A);
+    output = strcatrealloc(output, "\n");
+    return output;
 }
 
-void printHBorder(size_t h_border, FILE *output)
+char *printClose(size_t col, char *output)
+{
+
+    output = strcatrealloc(output, B_L_A);
+    for (size_t i = 0; i < col; i++)
+    {
+        output = strcatrealloc(output, H_L);
+    }
+    output = strcatrealloc(output, B_R_A);
+    output = strcatrealloc(output, "\n");
+    output = strcatrealloc(output, RESET);
+    return output;
+}
+
+char *printHBorder(size_t h_border, char *output)
 {
     for (size_t i = 0; i < h_border; i++)
-        fprintf(output, " ");
+        output = strcatrealloc(output, " ");
+    return output;
 }
 
-void printEmptyLine(size_t col, FILE *output)
+char *printEmptyLine(size_t col, char *output)
 {
-    fprintf(output, V_L);
+    output = strcatrealloc(output, V_L);
     for (size_t i = 0; i < col; i++)
-        fprintf(output, " ");
-    fprintf(output, V_L);
-    fputc('\n', output);
+        output = strcatrealloc(output, " ");
+    output = strcatrealloc(output, V_L);
+    output = strcatrealloc(output, "\n");
+    return output;
 }
 
-void printVBorder(size_t v_border, size_t col, FILE *output)
+char *printVBorder(size_t v_border, size_t col, char *output)
 {
     for (size_t i = 0; i < v_border; i++)
-        printEmptyLine(col, output);
+        output = printEmptyLine(col, output);
+    return output;
 }
 
 size_t strlen_unicode(char string[])
 {
+    if (!string)
+        return 0; // string NULL return size 0
+
     size_t i, tmh = 0;
     for (i = 0; string[i] != '\0'; ++i)
     {
@@ -78,11 +122,11 @@ size_t strlen_unicode(char string[])
     return tmh;
 }
 
-void printLine(char line[], size_t h_border, size_t maxArgsSize, FILE *output)
+char *printLine(char line[], size_t h_border, size_t maxArgsSize, char *output)
 {
-    fprintf(output, V_L);
-    printHBorder(h_border, output);
-    fprintf(output, "%s", line);
+    output = strcatrealloc(output, V_L);
+    output = printHBorder(h_border, output);
+    output = strcatrealloc(output, line);
     size_t line_size = strlen_unicode(line);
     size_t buffer;
     if (maxArgsSize > line_size)
@@ -93,9 +137,10 @@ void printLine(char line[], size_t h_border, size_t maxArgsSize, FILE *output)
     {
         buffer = h_border;
     }
-    printHBorder(buffer, output);
-    fprintf(output, V_L);
-    fputc('\n', output);
+    output = printHBorder(buffer, output);
+    output = strcatrealloc(output, V_L);
+    output = strcatrealloc(output, "\n");
+    return output;
 }
 
 size_t maxArgsSize(char **args)
@@ -113,9 +158,10 @@ size_t maxArgsSize(char **args)
     }
     return max;
 }
-void makeBox(char text[], size_t h_border, size_t v_border, FILE *output)
+char *makeBox(char text[], size_t h_border, size_t v_border, char *output)
 {
     char *text_p = strdup(text);
+    char *text_p_free = text_p;
     char **args = malloc(sizeof(char *) * 100);
     char *token;
     int i = 0, k = 0;
@@ -127,32 +173,41 @@ void makeBox(char text[], size_t h_border, size_t v_border, FILE *output)
     args[i] = NULL;
     size_t max_args_size = maxArgsSize(args);
     size_t col = max_args_size + (2 * h_border);
-    printHead(col, output);
-    printVBorder(v_border, col, output);
+    output = printHead(col, output);
+
+    output = printVBorder(v_border, col, output);
+
     while (args[k])
     {
-        printLine(args[k], h_border, max_args_size, output);
+        output = printLine(args[k], h_border, max_args_size, output);
         k++;
     }
-    printVBorder(v_border, col, output);
-    printClose(col, output);
+    output = printVBorder(v_border, col, output);
+    output = printClose(col, output);
+
+    // Free dynamic memory
+    free(text_p_free);
+    int j = 0;
+    while (args[j])
+    {
+        free(args[j]);
+        j++;
+    }
+    free(args);
+    return output;
 }
 
 int main()
 {
-    // printf("TMH: %d", strlen_unicode(BOLD_CYAN));
 
-    printf(BOLD_BLUE);
+    char *str = NULL;
 
-    makeBox(" MENU" RED UNICODE_X BOLD_BLUE
-             "\n1. Jogar" GREEN THIN_TICK BOLD_BLUE
-             "\n2. Editar "
-             "\n3. Sair " RED UNICODE_X UNICODE_X BOLD_BLUE,
-             2, 1, stdout);
-    printf(RESET);
 
-    makeBox(fruits[50],
-             2, 6, stdout);
-    makeBox(ball,
-             2, 2, stdout);
+    str = makeBox(" MENU" RED UNICODE_X BOLD_BLUE
+                  "\n1. Jogar" GREEN THIN_TICK BOLD_BLUE
+                  "\n2. Editar "
+                  "\n3. Sair " RED UNICODE_X UNICODE_X BOLD_BLUE,
+                  2, 1, str);
+    printf("%s", str);
+    free(str);
 }
