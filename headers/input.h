915759f9cdb8df54a3e62fcf98c15044278103d1
Gustominox <agstcmps.111@gmail.com>
2025-03-05 13:44:31 +0000

    tidy: move macros to .h

--- headers/input.h
+++ headers/input.h
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#define clearScreen() printf("\033[1;1H\033[2J")
+#define moveCursorTo(x, y) printf("\033[%d;%dH", (y), (x)); // Move cursor to (x, y)
 
 
 char getch();
