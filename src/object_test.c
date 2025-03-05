011a9332011c8fbab746daf02acdd2e567dc1193
Gustominox <agstcmps.111@gmail.com>
2025-03-05 14:37:17 +0000

    update: Add ball on test

--- src/object_test.c
+++ src/object_test.c
@@ -3,10 +3,31 @@
 #include "colors.h"
 #include "box.h"
 
+
+
+
+
 int main()
 {
 
-    char * output = makeBox("TEST TEST TEST\nTEST TEST\nTEST\n", 0, 0, 1);
+    char ball_art[] =
+"\n       _...----.._"
+"\n     ,:':::::.     `>."
+"\n   ,' |:::::;'     |:::."
+"\n  /    `'::'       :::::"
+"\n /         _____     `::;"
+"\n:         /:::::\\      ` :"
+"\n| ,.     /::FCP::\\       |"
+"\n|;:::.   `::::::;'       |"
+"\n::::::     `::;'      ,. ;"
+"\n \\:::'              ,::::/"
+"\n  \\                 \\:::/"
+"\n   `.     ,:.        :;'"
+"\n     `-.::::::..  _.''"
+"\n        ```----'''" ;
+
+
+    char * output = makeBox(ball_art, 0, 0, 6);
     printf("%s",output);
     
     return 0;
