bba00454ca58a4d0e952cd6ecc6a699a82eb9d16
Gustominox <agstcmps.111@gmail.com>
2025-03-05 13:45:28 +0000

    feat: Add arg for testing file in `build.sh`

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 set -xe
 
 # gcc -o $2 -ggdb -Wall -Wpedantic -I headers src/$1
-gcc -o program -ggdb -Wall -Wpedantic -I headers/ src/input.c src/box.c
+
+gcc -o program -ggdb -Wall -Wpedantic -I headers/ src/input.c src/box.c src/$1
