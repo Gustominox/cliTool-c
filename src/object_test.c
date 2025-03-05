da5aa28a1f4fea6d3290fea4f0960899e26220b2
Gustominox <agstcmps.111@gmail.com>
2025-03-05 13:51:26 +0000

    add: 2 test files

--- src/object_test.c
+++ src/object_test.c
@@ -6,30 +6,8 @@
 int main()
 {
 
-    MENU caixa = innitMenu();
-
-    MENU caixa2 = innitMenu();
-
-    setHVBorder(caixa, 2, 1);
-    // clearScreen();
-
-    addMenuItem(caixa, "   MENU");
-    addMenuItem(caixa, "1. Jogar" GREEN THIN_TICK RESET);
-    addMenuItem(caixa, "2. Editar ");
-    addMenuItem(caixa, "3. Sair " RED UNICODE_X RESET);
-
-    setHVBorder(caixa2, 2, 1);
-
-    addMenuItem(caixa2, "   MENU 2");
-    addMenuItem(caixa2, "1. Facil");
-    addMenuItem(caixa2, "2. Casual");
-    addMenuItem(caixa2, "3. Experiente");
-
-    addSubMenu(caixa, caixa2, 0);
-
-    menuLoop(caixa);
-
-    // freeMenu(caixa);
-
+    char * output = makeBox("TEST TEST TEST\nTEST TEST\nTEST\n", 0, 0, 1);
+    printf("%s",output);
+    
     return 0;
 }
